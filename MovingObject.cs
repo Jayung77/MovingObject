017751015c82eaed4f1255dd9c2e8c5545534e8c
jaejeongPark <sabc1178@gmail.com>
2023-07-14 19:02:34 +0900

    초기 커밋.

---
+++ MovingObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+	public float speed;
+
+	private Vector3 vector;
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") != 0) //상하좌우 방향키가 눌릴경우, or = ||
+		{
+			vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+			if (vector.x != 0)
+			{
+				transform.Translate(vector.x * speed, 0, 0);
+			}
+			else if (vector.y != 0)
+			{
+				transform.Translate(0, vector.y * speed, 0);
+			}
+		}
+
+	}
+}
